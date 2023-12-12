d1cdf4f5779832eb65282ac7cacf55e0ceb57e65
yeliz601 <yeliz601@BPC11>
2023-12-12 08:43:09 +0300

    Proje dosyası ekle.

---
+++ EkranaYazz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EkranaYazz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EkranIslem ei = new EkranIslem();
+            ei.EkranaYazz(10,0,5);
+            Console.WriteLine("======================");
+            ei.EkranaYazz();
+        }
+    }
+    class EkranIslem
+    {
+        public void EkranaYazz(params int[] sayilar)
+        {
+            if(sayilar.Length == 0)
+            {
+                Console.WriteLine("Parametre olmadığı için metottan çıkılıyor.");
+                return;
+            }
+            Console.WriteLine("Parametreden gelen değerler:");
+            foreach (var s in sayilar)
+            {
+                if (s == 0) return;
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
